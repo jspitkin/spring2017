8bef25d9eeb232380e123d543d434e49c2ada21a
Jake Pitkin <jakepitkin@gmail.com>
2017-03-26 13:39:15 -0600

    toml 8d

--- theory_of_machine_learning/hw2/code/prob8d.py
+++ theory_of_machine_learning/hw2/code/prob8d.py
@@ -1,4 +1,5 @@
 from random import uniform
+from random import randint
 import math
 
 def random_points(count):
@@ -16,8 +17,22 @@ def f(vec, points):
     return result
 
 
-def gradient(vec, points):
+def f_i(vec, point):
+    return math.pow(vec[0] - point[0], 2) + math.pow(vec[1] - point[1], 2)
 
+
+def gradient(vec, point):
+   grad = [0, 0]
+   grad[0] = 2 * vec[0] - point[0]
+   grad[1] = 2 * vec[1] - point[1]
+   return grad
+
+
+def step(vec, point, eta):
+    grad = gradient(vec, point)
+    grad[0] = vec[0] - grad[0] * eta
+    grad[1] = vec[1] - grad[1] * eta
+    return grad
 
 
 def optimum(points):
@@ -31,6 +46,74 @@ def optimum(points):
     return [x, y]
 
 
+def distance(vec_dist, opt_vec, points):
+    return abs(f(vec, points) - f(opt_vec, points))
+
+
+def update_eta(t):
+    return 1.0 / t
+
+
+# Experiment 1 - fixed value for eta
 eta = 0.5
-vec = [uniform(-1, 1), uniform(-1, 1)]
+average_count = 5
+T = 1000
+T_sum_10 = 0
+T_sum_100 = 0
+T_sum_1000 = 0
 points = random_points(100)
+opt_vec = optimum(points)
+
+for _ in range(average_count):
+    vec = [uniform(-100000, 100000), uniform(-100000, 100000)]
+    for t in range(1, T+1):
+        point = points[randint(0, len(points)-1)] 
+        vec = step(vec, point, eta)
+        if t == 10:
+            T_sum_10 += distance(vec, opt_vec, points)
+        elif t == 100:
+            T_sum_100 += distance(vec, opt_vec, points)
+        elif t == 1000:
+            T_sum_1000 += distance(vec, opt_vec, points)
+
+T_sum_10 = T_sum_10 / average_count
+T_sum_100 = T_sum_100 / average_count
+T_sum_1000 = T_sum_1000 / average_count
+
+print('Results averaged over', average_count, 'experiments with fixed eta.')
+print('Distance from optimum when T = 10:', T_sum_10)
+print('Distance from optimum when T = 100:', T_sum_100)
+print('Distance from optimum when T = 1000:', T_sum_1000)
+
+# Experiment 2 - dynamic value for eta
+eta = 0
+average_count = 5
+T = 1000
+T_sum_10 = 0
+T_sum_100 = 0
+T_sum_1000 = 0
+points = random_points(100)
+opt_vec = optimum(points)
+
+for _ in range(average_count):
+    vec = [uniform(-100000, 1000000), uniform(-100000, 100000)]
+    for t in range(1, T+1):
+        eta = update_eta(t)
+        point = points[randint(0, len(points)-1)] 
+        vec = step(vec, point, eta)
+        if t == 10:
+            T_sum_10 += distance(vec, opt_vec, points)
+        elif t == 100:
+            T_sum_100 += distance(vec, opt_vec, points)
+        elif t == 1000:
+            T_sum_1000 += distance(vec, opt_vec, points)
+
+T_sum_10 = T_sum_10 / average_count
+T_sum_100 = T_sum_100 / average_count
+T_sum_1000 = T_sum_1000 / average_count
+
+print()
+print('Results averaged over', average_count, 'experiments with 1/t eta.')
+print('Distance from optimum when T = 10:', T_sum_10)
+print('Distance from optimum when T = 100:', T_sum_100)
+print('Distance from optimum when T = 1000:', T_sum_1000)
